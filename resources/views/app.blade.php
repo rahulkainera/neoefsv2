eb562d1b9ac002de5306990a554bb41dbfc85933
rahulkainera <amohanty@unomaha.edu>
2017-02-25 00:59:31 -0600

    Update app.blade.php
      added <a href="{{ action('MutualfundController@index') }}">Mutualfunds</a>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,8 @@
 <div class="container">
     <a href="{{ action('CustomerController@index') }}">Customers</a> |
     <a href="{{ action('StockController@index') }}">Stocks</a> |
-    <a href="{{ action('InvestmentController@index') }}">Investments</a>
+    <a href="{{ action('InvestmentController@index') }}">Investments</a> |
+    <a href="{{ action('MutualfundController@index') }}">Mutualfunds</a>
 </div>
 <hr>
 <div class="container">
